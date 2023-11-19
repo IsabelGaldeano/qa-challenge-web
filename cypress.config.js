2bc555e57e608b861562903f39f19dba9b35b6e8
Isabel Garcia <>
2023-11-19 19:17:27 +0100

    Added videos configuration set to true

--- cypress.config.js
+++ cypress.config.js
@@ -14,6 +14,7 @@ module.exports = defineConfig({
         }))
         return config;
     },
-    specPattern: "cypress/e2e/features/*.feature"
+    specPattern: "cypress/e2e/features/*.feature",
+    video: true
   }
 })
